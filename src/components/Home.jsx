5444df9c15a24669dbbdb1a5fa91e47c7698ff6b
Lucdomingues <lojalucc@outlook.com>
2022-09-08 17:48:49 -0300

    requisito 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <section>
+        <Link to="/carrinho" data-testid="shopping-cart-button">
+          Carrinho de Compras
+        </Link>
         <ul>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
