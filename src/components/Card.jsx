a734bde257bc716a70f3d584fd0522e3d0157538
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-09-12 18:09:19 -0300

    requisito 11

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,12 @@ class Card extends React.Component {
     productName: '',
     productImage: '',
     productPrice: 0,
+    productId: '',
     fichaTecnica: [],
+    inputEmail: '',
+    textarea: '',
+    avaliationInfo: [],
+    inputValidation: false,
   };
 
   async componentDidMount() {
@@ -21,11 +26,51 @@ class Card extends React.Component {
       productImage: product.thumbnail,
       productPrice: product.price,
       fichaTecnica: product.attributes,
+      productId: product.id,
     });
+    const recoverInfo = await JSON.parse(localStorage.getItem(product.id));
+    console.log(recoverInfo);
+    this.setState({ avaliationInfo: recoverInfo });
+    if (!localStorage.getItem(product.id)) {
+      this.setState({ avaliationInfo: [] });
+    }
   }
 
+  handleEmail = ({ target }) => {
+    const { value } = target;
+    this.setState({ inputEmail: value });
+  };
+
+  handleText = ({ target }) => {
+    const { value } = target;
+    this.setState({ textarea: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { inputEmail,
+      textarea, productId, avaliationInfo } = this.state;
+    if (inputEmail.length < 1 && textarea.length < 1) {
+      this.setState({ inputValidation: true });
+    } else {
+      this.setState({ inputValidation: false });
+
+      const products = [...avaliationInfo];
+      const product = {
+        email: inputEmail,
+        text: textarea,
+      };
+      products.push(product);
+      localStorage.setItem(productId, JSON.stringify(products));
+      this.setState({ avaliationInfo: [...products], inputEmail: '', textarea: '' });
+    }
+  };
+
   render() {
-    const { productName, productImage, productPrice, fichaTecnica } = this.state;
+    const { productName,
+      productImage,
+      productPrice,
+      fichaTecnica, inputEmail, textarea, avaliationInfo, inputValidation } = this.state;
     return (
       <section>
         <Header />
@@ -64,6 +109,52 @@ class Card extends React.Component {
               Ir ao carrinho
             </Link>
           </article>
+        </section>
+        <section className="evaluation-form">
+          <form>
+            <label htmlFor="input-email">
+              <input
+                type="email"
+                value={ inputEmail }
+                data-testid="product-detail-email"
+                name="input-email"
+                id="input-email"
+                placeholder="Email"
+                onChange={ this.handleEmail }
+              />
+            </label>
+            <textarea
+              name=""
+              value={ textarea }
+              placeholder="Mensagem (opcional)"
+              id=""
+              data-testid="product-detail-evaluation"
+              cols="30"
+              rows="10"
+              onChange={ this.handleText }
+            />
+            <button
+              data-testid="submit-review-btn"
+              type="submit"
+              onClick={ this.handleSubmit }
+            >
+              Avaliar
+
+            </button>
+          </form>
+        </section>
+        {inputValidation && <p data-testid="error-msg">Campos inválidos</p>}
+        <section className="review">
+          <ul>
+            {avaliationInfo.map((item, index) => (
+              <li key={ index }>
+                <div>
+                  <p data-testid="review-card-email">{item.email}</p>
+                  <p data-testid="review-card-evaluation">{item.text}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
         </section>
       </section>
     );
